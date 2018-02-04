f66b7f46011c609134704890d6ba52d2b54ec63b
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2018-02-04 18:29:26 +0100

    back to basics

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('test').onclick = function () {
-    query()
+    sendQuery()
 };
 
 function testQuery() {
@@ -7,18 +7,40 @@ function testQuery() {
 }
 
 function query() {
-    var testQuery = 'PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 WHERE { ?beer_1 a n1:beer . } LIMIT 5';
+    var testQuery = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 ?label_22 WHERE { ?beer_1 a n1:beer . ?beer_1 rdfs:label ?label_22 . } LIMIT 5';
 
     var url = "http://localhost:3030/open-beer/query";
 
     var params = "testQuery";
     var http = new XMLHttpRequest();
 
-    http.open("GET", url + '?query=' + encodeURIComponent(testQuery), true);
+    http.open("GET", url + '?query=' + encodeURIComponent(testQuery) + '&format=json', true);
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
+            //JSONObject myObject = new JSONObject(http.responseText);
             console.log(http.responseText);
         }
     }
     http.send();
+}
+
+// url / query
+var sendQuery = function () {
+    var query = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 ?label_22 WHERE { ?beer_1 a n1:beer . ?beer_1 rdfs:label ?label_22 . } LIMIT 5';
+
+    var url = "http://localhost:3030/open-beer/sparql";
+
+    var queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
+    $.ajax({
+        dataType: "json", //jsonp
+        url: queryUrl,
+        success: function (result) {
+            // console.log("Done");
+            console.log(result);
+            // $("#divResult").html(result);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
 }
