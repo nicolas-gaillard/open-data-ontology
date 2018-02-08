9fba19bab7c0799bbbea0c94b1b1a4899b839729
Anthony GRIFFON <anthony.griffon@etu.univ-nantes.fr>
2018-02-08 21:57:38 +0100

    un peu plus clair mais encore à clean

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
-document.getElementById('test').onclick = function () {
-    sendQuery()
+document.getElementById("test").onclick = function() {
+  sendQuery();
 };
 
-document.getElementById("go").onclick = function () {
-    var url = 'http://localhost:3030/open-beer/sparql';
-    var prefix = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>'
-    query = buildQuery();
-    // console.log(query);
-    getSparqlData(prefix + query, url, 'brewer')
+document.getElementById("go").onclick = function() {
+  const url = "http://localhost:3030/open-beer/sparql";
+  var prefix = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1:
+    <http://beer.beer/data#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>`;
+  let query = buildQuery();
+  getSparqlData(prefix + query, url, "brewer");
 };
 
 function getBrewer(brewer) {
-    var url = 'http://localhost:3030/open-beer/sparql';
+  const url = "http://localhost:3030/open-beer/sparql";
 
-    brewId = brewer.split("#")[1]
-    console.log(brewId);
-
-    var query = `
+  let brewId = brewer.split("#")[1];
+  let query = `
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX n1: <http://beer.beer/data#>
     PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
     SELECT DISTINCT ?brewer ?name ?website ?address
     WHERE { ?brewer a n1:brewer .
             ?brewer rdfs:label ?name .
-  			?brewer n1:locate ?address .
-  			OPTIONAL { ?brewer  n1:website  ?website }
+        ?brewer n1:locate ?address .
+        OPTIONAL { ?brewer  n1:website  ?website }
     FILTER (?brewer = n1:${brewId})
-    }`
-
-    console.log(query)
-    getSparqlData(query, url, 'null');
+    }`;
+  getSparqlData(query, url, "null");
 }
 
 function buildQuery() {
-    query = `
+  const query = `
     SELECT DISTINCT ?beer ?name ?alcohol_degree ?SRM ?style ?brewer
     WHERE { ?beer a n1:beer .
             ?beer rdfs:label ?name .
@@ -43,198 +39,208 @@ function buildQuery() {
             ?brewer a n1:brewer .
     }
     LIMIT 20`;
-    // var val = $('#alcohol').slider("option", "value");
-    return query;
+  // var val = $('#alcohol').slider("option", "value");
+  return query;
 }
 
 function getSparqlData(query, url, button) {
-    var table = $("#results");
-    table.text(" ");
+  let table = $("#results");
+  table.text(" ");
 
-    // console.log(query);
-    var queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
-
-    console.log(queryUrl);
-
-    $.ajax({
-        dataType: "jsonp",
-        url: queryUrl,
-        success: function (data) {
-
-            // console.log(data);
-
-            // get the table element
-            var table = $("#results");
-            table.text();
-
-            // get the sparql variables from the 'head' of the data.
-            var headerVars = data.head.vars;
-
-            // using the vars, make some table headers and add them to the table;
-            var trHeaders = getTableHeaders(headerVars);
-            table.append(trHeaders);
-
-            // grab the actual results from the data.
-            var bindings = data.results.bindings;
-
-            cpt = 1;
-            // for each result, make a table row and add it to the table.
-            for (rowIdx in bindings) {
-                table.append(getTableRow(headerVars, bindings[rowIdx], cpt, button));
-                cpt++;
-            }
-
-            if (button == 'brewer') {
-                $(".seeBrewer").click(function () {
-                    getBrewer($(this).val());
-                });
-            }
-        },
-        error: function (error) {
-            console.log("Error occured" + error);
-        }
-    });
-};
+  let queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
+  $.ajax({
+    dataType: "jsonp",
+    url: queryUrl,
+    success: data => {
+      // get the table element
+      var table = $("#results");
+      table.text();
+      // get the sparql variables from the 'head' of the data.
+      var headerVars = data.head.vars;
+      // using the vars, make some table headers and add them to the table;
+      var trHeaders = getTableHeaders(headerVars);
+      table.append(trHeaders);
+      // grab the actual results from the data.
+      var bindings = data.results.bindings;
+      cpt = 1;
+      // for each result, make a table row and add it to the table.
+      for (rowIdx in bindings) {
+        table.append(getTableRow(headerVars, bindings[rowIdx], cpt, button));
+        cpt++;
+      }
+      if (button == "brewer") {
+        $(".seeBrewer").click(function() {
+          getBrewer($(this).val());
+        });
+      }
+    },
+    error: function(error) {
+      console.log("Error occured" + error);
+    }
+  });
+}
 
 function getTableHeaders(headerVars) {
-    var trHeaders = "<tr><th></th>";
-    for (var i in headerVars) {
-        trHeaders += "<th>" + headerVars[i] + "</th>";
-    }
-    trHeaders += "</tr>";
-    return trHeaders;
+  let trHeaders = "<tr><th></th>";
+  for (let i in headerVars) {
+    trHeaders += "<th>" + headerVars[i] + "</th>";
+  }
+  trHeaders += "</tr>";
+  return trHeaders;
 }
 
 function getTableRow(headerVars, rowData, cpt, button) {
-    var tr = "<tr><td>" + cpt + "</td>";
-    for (var i in headerVars) {
-        tr += getTableCell(headerVars[i], rowData);
-    }
+  var tr = "<tr><td>" + cpt + "</td>";
+  for (var i in headerVars) {
+    tr += getTableCell(headerVars[i], rowData);
+  }
 
-    if (button == 'brewer') {
-        tr += "<td><button value='" + rowData[headerVars[headerVars.length - 1]]["value"] + "' class='seeBrewer'>See brewer</button></td> </tr>";
-        // } else if (button == address) {
-        //     tr += "<td><button value='" + rowData[headerVars[0]]["value"] + "' class='seeMovieGenre'>Voir films</button></td> </tr>";
-    } else {
-        tr += "</tr>";
-    }
-    // tr += "</tr>";
-    return tr;
+  if (button == "brewer") {
+    tr +=
+      "<td><button value='" +
+      rowData[headerVars[headerVars.length - 1]]["value"] +
+      "' class='seeBrewer'>See brewer</button></td> </tr>";
+    // } else if (button == address) {
+    //     tr += "<td><button value='" + rowData[headerVars[0]]["value"] + "' class='seeMovieGenre'>Voir films</button></td> </tr>";
+  } else {
+    tr += "</tr>";
+  }
+  // tr += "</tr>";
+  return tr;
 }
 
 function getTableCell(fieldName, rowData) {
-    var td = "<td>";
-    var fieldData = rowData[fieldName];
-    if (fieldData == null) td += ''
-    else if (fieldData["value"].indexOf('#') !== -1) td += fieldData["value"].split("#")[1];
-    else td += fieldData["value"];
-    td += "</td>";
-    return td;
+  var td = "<td>";
+  var fieldData = rowData[fieldName];
+  if (fieldData == null) td += "";
+  else if (fieldData["value"].indexOf("#") !== -1)
+    td += fieldData["value"].split("#")[1];
+  else td += fieldData["value"];
+  td += "</td>";
+  return td;
 }
 
 // JQuery
 // ------
 
-$(function () {
-    $("#country").selectmenu();
+$(function() {
+  $("#country").selectmenu();
 
-    $("#brewers").selectmenu();
+  $("#brewers").selectmenu();
 
-    $("#category").selectmenu();
-    // .selectmenu("menuWidget")
-    // .addClass("overflow");
+  $("#category").selectmenu();
+  // .selectmenu("menuWidget")
+  // .addClass("overflow");
 
-    $("#slider-range-alc").slider({
-        range: true,
-        min: 0,
-        max: 100,
-        values: [0, 100],
-        slide: function (event, ui) {
-            $("#alcohol").val("°" + ui.values[0] + " - °" + ui.values[1]);
-        }
-    });
-    $("#alcohol").val("°" + $("#slider-range-alc").slider("values", 0) +
-        " - °" + $("#slider-range-alc").slider("values", 1));
+  $("#slider-range-alc").slider({
+    range: true,
+    min: 0,
+    max: 100,
+    values: [0, 100],
+    slide: function(event, ui) {
+      $("#alcohol").val("°" + ui.values[0] + " - °" + ui.values[1]);
+    }
+  });
+  $("#alcohol").val(
+    "°" +
+      $("#slider-range-alc").slider("values", 0) +
+      " - °" +
+      $("#slider-range-alc").slider("values", 1)
+  );
 
-    $("#slider-range-ibu").slider({
-        range: true,
-        min: 0,
-        max: 93,
-        values: [0, 93],
-        slide: function (event, ui) {
-            $("#ibu").val(ui.values[0] + " - " + ui.values[1] + " IBT");
-        }
-    });
-    $("#ibu").val($("#slider-range-ibu").slider("values", 0) +
-        " - " + $("#slider-range-ibu").slider("values", 1) + " IBT");
+  $("#slider-range-ibu").slider({
+    range: true,
+    min: 0,
+    max: 93,
+    values: [0, 93],
+    slide: function(event, ui) {
+      $("#ibu").val(ui.values[0] + " - " + ui.values[1] + " IBT");
+    }
+  });
+  $("#ibu").val(
+    $("#slider-range-ibu").slider("values", 0) +
+      " - " +
+      $("#slider-range-ibu").slider("values", 1) +
+      " IBT"
+  );
 
-    $("#slider-range-srm").slider({
-        range: true,
-        min: 0,
-        max: 47,
-        values: [0, 47],
-        slide: function (event, ui) {
-            $("#srm").val(ui.values[0] + " - " + ui.values[1] + " SRM");
-        }
-    });
-    $("#srm").val($("#slider-range-srm").slider("values", 0) +
-        " - " + $("#slider-range-srm").slider("values", 1) + " SRM");
+  $("#slider-range-srm").slider({
+    range: true,
+    min: 0,
+    max: 47,
+    values: [0, 47],
+    slide: function(event, ui) {
+      $("#srm").val(ui.values[0] + " - " + ui.values[1] + " SRM");
+    }
+  });
+  $("#srm").val(
+    $("#slider-range-srm").slider("values", 0) +
+      " - " +
+      $("#slider-range-srm").slider("values", 1) +
+      " SRM"
+  );
 });
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-function sendQuery(url, query) {
-    var queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
-    $.ajax({
-        dataType: 'jsonp', //jsonp
-        url: queryUrl,
-        success: function (data) {
-            console.log(data)
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
-}
-
-function query() {
-    var testQuery = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 ?label_22 WHERE { ?beer_1 a n1:beer . ?beer_1 rdfs:label ?label_22 . } LIMIT 5';
-
-    var url = "http://localhost:3030/open-beer/query";
-
-    var params = testQuery;
-    var http = new XMLHttpRequest();
-
-    http.open("GET", url + '?query=' + encodeURIComponent(testQuery) + '&format=json', true);
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            console.log(http.responseText);
-
-            test = JSON.parse(http.responseText);
-            console.log(test)
-        }
+/*
+ * function sendQuery(url, query) {
+  var queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
+  $.ajax({
+    dataType: "jsonp", //jsonp
+    url: queryUrl,
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(error) {
+      console.log(error);
     }
-    http.send();
+  });
 }
+*/
+
+/* function testQuery() {
+  const testQuery =
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 ?label_22 WHERE { ?beer_1 a n1:beer . ?beer_1 rdfs:label ?label_22 . } LIMIT 5";
+
+  const url = "http://localhost:3030/open-beer/query";
+  let http = new XMLHttpRequest();
+
+  http.open(
+    "GET",
+    url + "?query=" + encodeURIComponent(testQuery) + "&format=json",
+    true
+  );
+  http.onreadystatechange = () => {
+    if (http.readyState == 4 && http.status == 200) {
+      console.log(http.responseText);
+
+      let test = JSON.parse(http.responseText);
+      console.log(test);
+    }
+  };
+  http.send();
+}*/
 
 // url / query
-var sendQuery = function () {
-    var query = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 ?label_22 WHERE { ?beer_1 a n1:beer . ?beer_1 rdfs:label ?label_22 . } LIMIT 5';
+let sendQuery = function() {
+  let query =
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <http://beer.beer/data#> SELECT DISTINCT ?beer_1 ?label_22 WHERE { ?beer_1 a n1:beer . ?beer_1 rdfs:label ?label_22 . } LIMIT 5";
 
-    var url = 'http://localhost:3030/open-beer/sparql';
+  let url = "http://localhost:3030/open-beer/sparql";
 
-    var queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
-    $.ajax({
-        dataType: 'jsonp', //jsonp
-        url: queryUrl,
+  let queryUrl = url + "?query=" + encodeURIComponent(query) + "&format=json";
+  $.ajax({
+    dataType: "jsonp", //jsonp
+    url: queryUrl,
 
-        success: function (data) {
-            console.log(data)
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
-}
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+};
