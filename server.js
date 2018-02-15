c1bd66df1f66a3e13f33cc4e11073d4303495d8b
Anthony GRIFFON <anthony.griffon@etu.univ-nantes.fr>
2018-02-15 16:48:52 +0100

    simple deploy

---
+++ server.js
@@ -0,0 +1,20 @@
+// simple express server for HTML pages!
+// ES6 style
+
+const express = require("express");
+const fs = require("fs");
+const hostname = "127.0.0.1";
+const port = 80;
+const app = express();
+app.use(express.static(__dirname + "/"));
+
+app.get("/", function(request, response) {
+  response.sendfile("index.html");
+});
+
+app.listen(port, () => {
+  console.log(`
+        Server is running at http://${hostname}:${port}/ 
+        Server hostname ${hostname} is listening on port ${port}!
+    `);
+});
